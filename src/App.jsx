3eb2f4d60ab5e09ab8837fd43fa0fb21d817daf6
alantsaji18 <alantsaji10@gmail.com>
2026-09-05 14:10:08 +0530

    Mini Project updates

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import AdminLogin from './components/AdminLogin';
 import StudentLogin from './components/StudentLogin';
 import StaffLogin from './components/StaffLogin';
 import StudentDashboard from './components/StudentDashboard';
+import TravelHistory from './components/Travelhistory';
 import "leaflet/dist/leaflet.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -29,6 +30,7 @@ function App() {
         <Route path="/adminlogin" element={<AdminLogin />} />
         <Route path="/studentlogin" element={<StudentLogin />} />
         <Route path="/stafflogin" element={<StaffLogin />} />
+        <Route path="/travelhistory" element={<TravelHistory />} />
       </Routes>
     </BrowserRouter>
   );
